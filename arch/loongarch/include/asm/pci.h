e67cd3c9daddf727e84be04c69d157824427e60b
AAAwlx <561639418@qq.com>
2024-04-03 22:32:27 +0800

    添加pci相关的头文件

--- arch/loongarch/include/asm/pci.h
+++ arch/loongarch/include/asm/pci.h
@@ -3,68 +3,106 @@
 #include <./include/linux/types.h>
 #include<asm-generic/io.h>
 
-// pci设备结构的通用标题字段
-struct pci_device_structure_header_t
+#define PCI_CONFIG0_BASE 0xfe00000000	/*PCI配置空间数据端口,type0一般设备*/
+#define PCI_CONFIG1_BASE 0xfe10000000   /*PCI配置空间数据端口,type1 PCI Bridge*/
+
+#define PCI_MAX_BAR 6		/*每个设备最多有6地址信息*/
+#define PCI_MAX_BUS 256		/*PCI总共有256个总线*/
+#define PCI_MAX_DEV 32		/*PCI每条总线上总共有32个设备*/
+#define PCI_MAX_FUN 8		/*PCI设备总共有8个功能号*/
+#define PCI_MAX_DEVICE_NR 256	/*系统最大支持检测多少个设备*/
+
+/*PCI配置空间数据的偏移*/
+#define PCI_DEVICE_VENDER								0x00	
+#define PCI_STATUS_COMMAND								0x04	
+#define PCI_CLASS_CODE_REVISION_ID						0x08	
+#define PCI_BIST_HEADER_TYPE_LATENCY_TIMER_CACHE_LINE	0x0C
+#define PCI_BASS_ADDRESS0								0x10
+#define PCI_BASS_ADDRESS1								0x14
+#define PCI_BASS_ADDRESS2								0x18
+#define PCI_BASS_ADDRESS3								0x1C
+#define PCI_BASS_ADDRESS4								0x20
+#define PCI_BASS_ADDRESS5								0x24
+#define PCI_CARD_BUS_POINTER							0x28
+#define PCI_SUBSYSTEM_ID							    0x2C
+#define PCI_EXPANSION_ROM_BASE_ADDR                     0x30
+#define PCI_CAPABILITY_LIST                             0x34
+#define PCI_RESERVED							        0x38
+#define PCI_MAX_LNT_MIN_GNT_IRQ_PIN_IRQ_LINE			0x3C
+
+/*向command中写入的控制位*/
+#define  PCI_COMMAND_IO		0x1	/* I/O 空间的响应使能 */
+#define  PCI_COMMAND_MEMORY	0x2	/* 内存空间的响应使能 */
+#define  PCI_COMMAND_MASTER	0x4	/* 总线主控制使能 */
+#define  PCI_COMMAND_SPECIAL	0x8	/* 特殊事务响应使能 */
+#define  PCI_COMMAND_INVALIDATE	0x10	/* 使用内存写入并且使其失效 */
+#define  PCI_COMMAND_VGA_PALETTE 0x20	/* 启用VGA调色板的监听 */
+#define  PCI_COMMAND_PARITY	0x40	/* 启用奇偶校验检查 */
+#define  PCI_COMMAND_WAIT	0x80	/* 启用地址/数据步进 */
+#define  PCI_COMMAND_SERR	0x100	/* 启用系统错误（SERR） */
+#define  PCI_COMMAND_FAST_BACK	0x200	/* 启用快速回写 */
+#define  PCI_COMMAND_INTX_DISABLE 0x400 /* INTx模拟禁用 */
+
+/*IO地址和MEM地址的地址mask*/
+#define PCI_BASE_ADDR_MEM_MASK           (~0x0FUL)
+#define PCI_BASE_ADDR_IO_MASK            (~0x03UL)
+
+/* PCI设备ID */
+struct pci_device_id
 {
-    //struct List list;
-    // ==== 以下三个变量表示该结构体所处的位置
-    uint8_t bus;
-    uint8_t device;
-    uint8_t func;
+	uint32_t vendor, device;   //供应商和设备 ID 或 PCI_ANY_ID
+	uint32_t subvendor, subdevice;   //子系统的 id 或 PCI_ANY_ID
+	uint32_t device_class, class_mask;   //
+};
 
-    uint16_t Vendor_ID; // 供应商ID 0xffff是一个无效值，在读取访问不存在的设备的配置空间寄存器时返回
-    uint16_t Device_ID; // 设备ID，标志特定设备
-
-    uint16_t Command; // 提供对设备生成和响应pci周期的能力的控制 向该寄存器写入0时，设备与pci总线断开除配置空间访问以外的所有连接
-    uint16_t Status;  // 用于记录pci总线相关时间的状态信息寄存器
-
-    uint8_t RevisionID; // 修订ID，指定特定设备的修订标志符
-    uint8_t ProgIF;     // 编程接口字节，一个只读寄存器，指定设备具有的寄存器级别的编程接口（如果有的话）
-    uint8_t SubClass;   // 子类。指定设备执行的特定功能的只读寄存器
-    uint8_t Class_code; // 类代码，一个只读寄存器，指定设备执行的功能类型
-
-    uint8_t CacheLineSize; // 缓存线大小：以 32 位为单位指定系统缓存线大小。设备可以限制它可以支持的缓存线大小的数量，如果不支持的值写入该字段，设备将表现得好像写入了 0 值
-    uint8_t LatencyTimer;  // 延迟计时器：以 PCI 总线时钟为单位指定延迟计时器。
-    uint8_t HeaderType;    // 标头类型 a value of 0x0 specifies a general device, a value of 0x1 specifies a PCI-to-PCI bridge, and a value of 0x2 specifies a CardBus bridge. If bit 7 of this register is set, the device has multiple functions; otherwise, it is a single function device.
-    uint8_t BIST;          // Represents that status and allows control of a devices BIST (built-in self test).
-                           // Here is the layout of the BIST register:
-                           // |     bit7     |    bit6    | Bits 5-4 |     Bits 3-0    |
-                           // | BIST Capable | Start BIST | Reserved | Completion Code |
-                           // for more details, please visit https://wiki.osdev.org/PCI
-} __attribute__((packed));
-
-/**
- * @brief 表头类型为0x0的pci设备结构,一般的pci设备
- *
- */
-
-struct pci_device_structure_general_device_t
+/*PCI地址bar结构体，保存Bass Address （0~5）的信息*/
+typedef struct pci_device_bar
 {
-    struct pci_device_structure_header_t header;
-    uint32_t BAR0;
-    uint32_t BAR1;
-    uint32_t BAR2;
-    uint32_t BAR3;
-    uint32_t BAR4;
-    uint32_t BAR5;
-    uint32_t Cardbus_CIS_Pointer; // 指向卡信息结构，供在 CardBus 和 PCI 之间共享芯片的设备使用。
+	unsigned int type;		    /*地址bar的类型（IO地址/MEM地址）*/
+    unsigned int base_addr;	    /*地址的值*/
+    unsigned int length;	    /*地址的长度*/
+} pci_device_bar_t;
 
-    uint16_t Subsystem_Vendor_ID;
-    uint16_t Subsystem_ID;
+#define PCI_DEVICE_INVALID 		0/*被定义为 0，表示无效的PCI设备*/
+#define PCI_DEVICE_USING		1/*被定义为 1，表示正在使用的PCI设备*/
 
-    uint32_t Expansion_ROM_base_address;
+/*PCI BAR的类型*/
+#define PCI_BAR_TYPE_INVALID 	0
+#define PCI_BAR_TYPE_MEM 		1
+#define PCI_BAR_TYPE_IO 		2
 
-    uint8_t Capabilities_Pointer;
-    uint8_t reserved0;
-    uint16_t reserved1;
-
-    uint32_t reserved2;
-
-    uint8_t Interrupt_Line; // 指定设备的中断引脚连接到系统中断控制器的哪个输入，并由任何使用中断引脚的设备实现。对于 x86 架构，此寄存器对应于 PIC IRQ 编号 0-15（而不是 I/O APIC IRQ 编号），并且值0xFF定义为无连接。
-    uint8_t Interrupt_PIN;  // 指定设备使用的中断引脚。其中值为0x1INTA#、0x2INTB#、0x3INTC#、0x4INTD#，0x0表示设备不使用中断引脚。
-    uint8_t Min_Grant;      // 一个只读寄存器，用于指定设备所需的突发周期长度（以 1/4 微秒为单位）（假设时钟速率为 33 MHz）
-    uint8_t Max_Latency;    // 一个只读寄存器，指定设备需要多长时间访问一次 PCI 总线（以 1/4 微秒为单位）。
-} __attribute__((packed));
-
-
+/*
+PCI设备结构体，用于保存我们所需要的pci信息，并不是和硬件的一样
+*/
+typedef struct pci_device
+{
+	int flags; 		/*device flags*/
+	
+	unsigned char bus;				/*bus总线号*/
+	unsigned char dev;				/*device号*/
+	unsigned char function;		/*功能号*/
+	
+	unsigned short vendor_id;		/*配置空间:Vendor ID*/
+    unsigned short device_id;		/*配置空间:Device ID*/
+    unsigned short command;		    /*配置空间:Command*/
+    unsigned short status;		    /*配置空间:Status*/
+    
+    unsigned int class_code;		/*配置空间:Class Code*/
+	unsigned char revision_id;		/*配置空间:Revision ID*/
+    unsigned char multi_function;	/*多功能标志*/
+    unsigned int card_bus_pointer;
+    unsigned short subsystem_vendor_id;
+    unsigned short subsystem_device_id;
+    unsigned int expansion_rom_base_addr;
+    unsigned int capability_list;
+    
+    unsigned char irq_line;			/*配置空间:IRQ line*/
+    unsigned char irq_pin;			/*配置空间:IRQ pin*/
+    unsigned char min_gnt;
+    unsigned char max_lat;
+    pci_device_bar_t bar[PCI_MAX_BAR];	/*有6个地址信息*/
+} pci_device_t;
 #endif
+
+
+void init_pci();//初始化pci
