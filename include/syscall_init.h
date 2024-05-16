821321ca987dd3aaac52523561b31113e68c1aa2
AAAwlx <561639418@qq.com>
2024-05-16 14:41:21 +0800

    修改sys_write

--- include/syscall_init.h
+++ include/syscall_init.h
@@ -4,7 +4,6 @@
 #include <linux/thread.h>
 
 pid_t sys_getpid(void);
-int64_t sys_write(int fd,const void* buf,size_t count);
 void sys_pstr(char *str);   //权宜之计，暂时写这儿
 void syscall_init(void);
 
--- kernel/syscall_init.c
+++ kernel/syscall_init.c
@@ -4,20 +4,12 @@
 #include <asm/stdio.h>
 #include <fork.h>
 #include <linux/console.h>
+#include <fs/syscall_fs.h>
 
 pid_t sys_getpid(void)
 {
 	struct task_struct* cur = running_thread();
 	return cur->pid;
-}
-
-int64_t sys_write(int fd,const void* buf,size_t count)
-{
-	const char* buffer = buf;
-	for (size_t i = 0; i < count; i++) {
-		console_put_char(buffer[i]);
-	}
-	return count;
 }
 
 void sys_pstr(char *str)
