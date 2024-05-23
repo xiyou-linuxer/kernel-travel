d3463632b4c583bd211bb7544b342a6ec44c3f94
zhendewokusi <768672249@qq.com>
2024-05-24 02:50:38 +0800

    为 compile.sh 添加工具链

--- command/compile.sh
+++ command/compile.sh
@@ -2,8 +2,14 @@ BIN="user_prog"
 CFLAGS="-c -g  -mabi=lp64s"
 OBJS="../arch/loongarch/kernel/syscall.o "
 
+CC="/opt/gcc-13.2.0-loongarch64-linux-gnu/bin/loongarch64-linux-gnu-gcc"
+LD="/opt/gcc-13.2.0-loongarch64-linux-gnu/bin/loongarch64-linux-gnu-ld"
+
+# CC="../cross-tools/bin/loongarch64-unknown-linux-gnu-gcc"
+# LD="../cross-tools/bin/loongarch64-unknown-linux-gnu-ld"
+
 rm *.o
 rm user_prog
-/opt/gcc-13.2.0-loongarch64-linux-gnu/bin/loongarch64-linux-gnu-gcc $CFLAGS -I "../arch/loongarch/include/" -I "../include" -o printf.o printf.c
-/opt/gcc-13.2.0-loongarch64-linux-gnu/bin/loongarch64-linux-gnu-gcc $CFLAGS -I "../arch/loongarch/include/" -I "../include" -o $BIN".o" $BIN".c"
-/opt/gcc-13.2.0-loongarch64-linux-gnu/bin/loongarch64-linux-gnu-ld -e main -T program.ld printf.o $BIN".o" $OBJS -o $BIN
+$CC $CFLAGS -I "../arch/loongarch/include/" -I "../include" -o printf.o printf.c
+$CC $CFLAGS -I "../arch/loongarch/include/" -I "../include" -o $BIN".o" $BIN".c"
+$LD -e main -T program.ld printf.o $BIN".o" $OBJS -o $BIN
