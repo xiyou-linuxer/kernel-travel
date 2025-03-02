08536ad0c5c918c1414268bac760a90b5ad98d8d
huloves <17791627852@163.com>
2025-03-02 17:54:47 +0800

    适配init_code生成流程

--- command/compile.sh
+++ command/compile.sh
@@ -1,14 +1,15 @@
-BIN="user_prog"
-CFLAGS="-c -g  -mabi=lp64s"
+BIN="init_code"
+CFLAGS="-c -g  -mabi=lp64s -D__KERNEL__"
 OBJS="../arch/loongarch/kernel/syscall.o "
 
 CC="/opt/gcc-13.2.0-loongarch64-linux-gnu/bin/loongarch64-linux-gnu-gcc"
 LD="/opt/gcc-13.2.0-loongarch64-linux-gnu/bin/loongarch64-linux-gnu-ld"
 
 rm *.o
-rm user_prog
+rm init_code
 $CC $CFLAGS -I "../arch/loongarch/include/" -I "../include" -o printf.o printf.c
 $CC $CFLAGS -I "../arch/loongarch/include/" -I "../include" -o $BIN".o" $BIN".c"
 $LD -e main -T program.ld printf.o $BIN".o" $OBJS -o $BIN
 #$CC -static -g -o $BIN $BIN.c
 
+xxd -i init_code ../include/xkernel/initcode.h
--- makeinit.sh
+++ makeinit.sh
@@ -1,7 +1,6 @@
 cd command
 ./compile.sh
 cd ..
-xxd  -n init_code -i command/user_prog include/xkernel/initcode.h
 make all
 #docker cp kernel.bin os-contest:/srv/tftp/kernel.bin
 #docker cp vmlinux os-contest:/vmlinux
