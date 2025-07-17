8f15d4e3d18ba670b1e1da4311059176e89483ef
hanyu0000 <3634802280.l@gmail.com>
2025-07-17 21:50:38 +0800

    early_setup

--- arch/riscv/kernel/setup.c
+++ arch/riscv/kernel/setup.c
@@ -5,6 +5,27 @@
 #include <xkernel/types.h>
 #include <xkernel/fdt.h>
 #include <xkernel/printk.h>
+
+#define COMMAND_LINE_SIZE	512
+#define FIX_BTMAPS_SLOTS	7
+#define NR_FIX_BTMAPS 2  // 每个slot占用的fixmap条目数，具体值依据平台
+#define FIXADDR_TOP      0xfffff000UL
+#define PAGE_SHIFT       12
+#define __fix_to_virt(x) (FIXADDR_TOP - ((x) << PAGE_SHIFT))
+#define FIX_BTMAP_BEGIN  50  // 你可以根据需要定义这个索引值
+static void *prev_map[FIX_BTMAPS_SLOTS];
+static void *slot_virt[FIX_BTMAPS_SLOTS];
+
+char  boot_command_line[COMMAND_LINE_SIZE];
+void  early_ioremap_setup(void)
+{
+	int i;
+
+	for (i = 0; i < FIX_BTMAPS_SLOTS; i++) {
+		//WARN_ON_ONCE(prev_map[i]);   逻辑未完成
+		slot_virt[i] = __fix_to_virt(FIX_BTMAP_BEGIN - NR_FIX_BTMAPS*i);
+	}
+}
 unsigned long boot_cpu_hartid;
 
 static void __init parse_dtb(void);
@@ -13,9 +34,7 @@ void setup_arch(void)
 {
 	// 解析设备树
 	parse_dtb();
-
-
-
+	early_ioremap_setup();//早期内存映射（early ioremap）机制的初始化函数
 
 	return;
 }
