9aa1891680c9735d49cd76e58486af47b1620cc8
hanyu0000 <3634802280.l@gmail.com>
2025-07-18 10:37:27 +0800

    trap_init

---
+++ arch/riscv/kernel/traps.c
@@ -0,0 +1,12 @@
+#include<asm/bootinfo.h>
+#include <asm/asm.h>
+
+#include <xkernel/init.h>
+#include <xkernel/types.h>
+#include <xkernel/fdt.h>
+#include <xkernel/printk.h>
+
+extern void trap_init(void);
+void trap_init(void){
+
+}
